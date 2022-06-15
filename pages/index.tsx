1052d2e7771bb4a4344df19f9296e10c75e71491
nik1168 <ngeisser32@gmail.com>
2022-06-14 20:52:18 -0400

    feat: add initial project structure

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import type {NextPage} from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Twitter clone</title>
+      </Head>
+        <main>
+            {/*sidebar*/}
+
+            {/*feed*/}
+            {/*widget*/}
+        </main>
+
+
+      <h1>Hey Nik</h1>
+
+    </div>
+  )
+}
+
+export default Home
