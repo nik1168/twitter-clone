670491202494b5398b03be9d26f4c2e9ec66a7be
nik1168 <ngeisser32@gmail.com>
2022-07-04 22:02:25 -0400

    feat: add mock files for tweets

---
+++ lib/__data__/mock.ts
@@ -0,0 +1,29 @@
+import {Tweet} from '../../typings';
+
+export const mockTweet: Tweet = {
+  _id: '1',
+  _rev: '',
+  _createdAt: '',
+  _updatedAt: '',
+  _type: 'tweet',
+  image: '',
+  username: 'nik1168',
+  text: 'Niks tweet',
+  profileImg: '',
+  blockTweet: false,
+};
+
+export const getMockTweet = (
+  id: string,
+  username: string,
+  text: string,
+  blockTweet: boolean,
+): Tweet => {
+  return {...mockTweet, _id: id, username, text, blockTweet};
+};
+
+export const mockTweets: Tweet[] = [
+  getMockTweet('1', 'nik1168', 'nik', false),
+  getMockTweet('2', 'nik1168', 'niko', false),
+  getMockTweet('3', 'nik1168', 'tweet 3', false),
+];
