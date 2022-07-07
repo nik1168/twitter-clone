83f53d5cbed749ada9447c8af21cc33e4038d1fd
nik1168 <ngeisser32@gmail.com>
2022-07-06 23:56:49 -0400

    feat: add env variables

--- lib/fetchComments.ts
+++ lib/fetchComments.ts
@@ -3,8 +3,14 @@ import fetch from 'cross-fetch';
 import {Comment} from '../typings';
 import {BASE_URL} from './constants';
 
-export const fetchComments = async (tweetId: string): Promise<Comment[]> => {
-  const res = await fetch(`${BASE_URL}/api/getComments?tweetId=${tweetId}`);
+export const fetchComments = async (
+  tweetId: string,
+): Promise<Comment[] | null> => {
+  try {
+    const res = await fetch(`${BASE_URL}/api/getComments?tweetId=${tweetId}`);
 
-  return res.json();
+    return await res.json();
+  } catch (ex) {
+    return null;
+  }
 };
