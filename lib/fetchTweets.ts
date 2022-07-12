0d7755a92254bba89c3beefdcc371d590cded6e8
nik1168 <ngeisser32@gmail.com>
2022-07-12 00:03:23 -0400

    feature: downgrade next auth

--- lib/fetchTweets.ts
+++ lib/fetchTweets.ts
@@ -4,6 +4,8 @@ import {Tweet} from '../typings';
 import {BASE_URL} from './constants';
 
 export const fetchTweets = async (): Promise<Tweet[]> => {
+  console.log('/api/getTweets`');
+  console.log(`${BASE_URL}/api/getTweets`);
   const res = await fetch(`${BASE_URL}/api/getTweets`);
   const data = await res.json();
 
